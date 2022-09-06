f09a9ccfbc31909d60ff70c7c35e967b2de13ad2
Tomash Sidei <tomash.sidei@ibm.com>
2022-09-06 19:08:17 +0300

    Part 3. Instead of storing the number of holes a different approach with calculating the number each time to not have a second source of truth which error prone and should be avoided when possible.

--- proxx/cell.go
+++ proxx/cell.go
@@ -18,6 +18,10 @@ func (c *cell) nearHole() bool {
 	return false
 }
 
+// surroundingBlackHolesNum don't store the number of adjacent black holes
+// that approach was chosen because it's not a heavy operation, it's always at most 8 iterations
+// but eliminate storing of another source of truth. This way you don't have to think of keeping the references
+// and the count in sync and can implement logic of moving holes around without calculating the count
 func (c *cell) surroundingBlackHolesNum() int {
 	num := 0
 	for _, surroundingCell := range c.SurroundingCells {
